24c47a845c0211ecedb1afded37bcd3429e46e3c
Ludmilla <ludmillajudaico@gmail.com>
2025-08-02 19:23:14 -0300

    imagem adicionada

--- routes.py
+++ routes.py
@@ -1,6 +1,10 @@
-from flask import render_template
+from flask import render_template, url_for
 from app import app
 
 @app.route("/")
 def homepage():
     return render_template("musicotecahome.html")
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
