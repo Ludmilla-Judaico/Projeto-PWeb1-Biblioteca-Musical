87798cfa34122a25fb9d2e7d03ba367d87ff6cc0
pprtbia <biancarb1421@gmail.com>
2025-08-14 20:57:37 -0300

    Base do html

--- app.py
+++ app.py
@@ -1,15 +1,3 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
-
-# @app.route('/')
-# def homepage():
-#     return render_template('musicotecahome.html')
-
-# @app.route('/profile')
-# def profile():
-#     return render_template('profile.html')
-
-# @app.route('/album')
-# def album():
-#     return render_template('descricao_album.html')
