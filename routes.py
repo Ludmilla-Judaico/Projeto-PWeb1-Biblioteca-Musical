2dc40dad723cbf2027a8123a86891c9b1d576c42
unknown <luis.carvalho@academico.ifpb.edu.br>
2025-08-17 18:55:21 -0300

    Criação do css e rota para a página de login

--- routes.py
+++ routes.py
@@ -12,3 +12,7 @@ def profile():
 @app.route('/album')
 def album():
     return render_template('descricao_album.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
