c9bd36264b146680919dc7c17d7469770a76813d
Ludmilla <ludmillajudaico@gmail.com>
2025-08-23 19:57:40 -0300

    Secret key

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from .main import app as app_bp
 
 def create_app():
     app = Flask(__name__)
-    app.secret_key = 'segredinho'
+    app.secret_key = 'secret'
 
     app.register_blueprint(app_bp)
 
